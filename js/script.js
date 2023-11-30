0b8dab42f8e752e51ad4f919d67a0c743dff478a
domacheche <bochenekdoma@gmail.com>
2023-11-30 11:49:16 +0100

    update

--- js/script.js
+++ js/script.js
@@ -1,29 +1,41 @@
 'use strict';
 
 function titleClickHandler(event){
+    event.preventDefault();
   console.log('Link was clicked!');
+  const clickedElement = this;
   console.log('Event object:', event);
 
-  /* remove class 'active' from all article links  */
+  /* [DONE] remove class 'active' from all article links  */
   const activeLinks = document.querySelectorAll('.titles a.active');
 
 for(let activeLink of activeLinks){
   activeLink.classList.remove('active');
 }
 
-  /* add class 'active' to the clicked link */
+  /*[DONE] add class 'active' to the clicked link */
+  clickedElement.classList.add('active');
 
-  /* remove class 'active' from all articles */
+  /* [DONE] remove class 'active' from all articles */
+  const activeArticles = document.querySelectorAll('.titles a.active');
+
+for(let activeArticle of activeArticles){
+  activeArticle.classList.remove('active');
+}
 
   /* get 'href' attribute from the clicked link */
+  const articleSelector = clickedElement.getAttribute('href');
 
   /* find the correct article using the selector (value of 'href' attribute) */
+  const targetArticle = document.querySelector(articleSelector);
 
   /* add class 'active' to the correct article */
+  targetArticle.classList.add('active');
 }
 
 const links = document.querySelectorAll('.titles a');
 
 for(let link of links){
   link.addEventListener('click', titleClickHandler);
+  
 }
